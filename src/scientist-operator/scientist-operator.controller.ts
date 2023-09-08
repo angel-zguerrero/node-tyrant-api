e76039482b1d5d13e8466d120b455849b5a2d030
Angel Zambrano <angel.zguerrero@gmail.com>
2023-09-08 10:04:25 +0000

    Move redis to service

--- src/scientist-operator/scientist-operator.controller.ts
+++ src/scientist-operator/scientist-operator.controller.ts
@@ -1,14 +1,12 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ScientistOperatorService } from './scientist-operator.service';
-import Redis from 'ioredis';
 import { CreateScientistOperationDto } from './dtos/scientist-operation.dto';
 
 @Controller('scientist-operator')
 export class ScientistOperatorController {
-  constructor(private readonly scientistOperatorService: ScientistOperatorService, @Inject('REDIS_CLIENT') private readonly redisConnector: Redis) { }
+  constructor(private readonly scientistOperatorService: ScientistOperatorService) { }
   @Post('solve')
   async solve(@Body() scientistOperation: CreateScientistOperationDto): Promise<object> {
-    await this.redisConnector.incr("scientist-operations-counter")
     return {
       register: (await this.scientistOperatorService.register(scientistOperation)),
       publish: (await this.scientistOperatorService.publish(scientistOperation))
--- src/scientist-operator/scientist-operator.service.ts
+++ src/scientist-operator/scientist-operator.service.ts
@@ -1,16 +1,17 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { ScientistOperation, ScientistOperationDocument } from './schemas/scientist-operation.schema';
+import { ScientistOperation } from './schemas/scientist-operation.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { CreateScientistOperationDto } from './dtos/scientist-operation.dto';
 import { ClientProxy, RmqRecordBuilder } from '@nestjs/microservices';
-
+import Redis from 'ioredis';
 @Injectable()
 export class ScientistOperatorService {
 
-  constructor(@InjectModel(ScientistOperation.name) private scientistOperationModel: Model<ScientistOperation>, @Inject('SCIENTIST_OPERATOR_SERVICE') private readonly scientistOperatorConnector: ClientProxy) { }
+  constructor(@InjectModel(ScientistOperation.name) private scientistOperationModel: Model<ScientistOperation>, @Inject('SCIENTIST_OPERATOR_SERVICE') private readonly scientistOperatorConnector: ClientProxy, @Inject('REDIS_CLIENT') private readonly redisConnector: Redis) { }
 
   async register(scientistOperationDto: CreateScientistOperationDto): Promise<ScientistOperation> {
+    await this.redisConnector.incr("scientist-operations-counter")
     const createdScientistOperation = new this.scientistOperationModel(scientistOperationDto);
     return await createdScientistOperation.save();
   }
